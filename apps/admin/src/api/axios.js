b50812884a1590a480b8bb7501d89db0ef0c7469
Animesh Kumar <kumaranimesh923@gmail.com>
2025-10-29 15:48:32 +0530

    fix: change backend api url

--- apps/admin/src/api/axios.js
+++ apps/admin/src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const apiUrl = "http://localhost:3000";
+export const apiUrl = import.meta.env.BACKEND_API_URL;
 
 const axiosInstance = axios.create({
   baseURL: apiUrl,
