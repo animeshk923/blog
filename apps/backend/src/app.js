4134a3e2d1409eca78f9cf867679080198cd3217
Animesh Kumar <kumaranimesh923@gmail.com>
2025-10-29 15:15:12 +0530

    chore: update port env (backend)

--- apps/backend/src/app.js
+++ apps/backend/src/app.js
@@ -2,23 +2,18 @@ require("dotenv").config();
 
 const express = require("express");
 const cors = require("cors");
-// const { localStrategyConfig } = require("./auth/passport");
 const routes = require("./routes");
 const { handleNonExistentRoutes } = require("./controllers/auth");
-
+const port = process.env.PORT || 3000;
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(express.json());
-// app.use(passport.session());
-
-// passport.use(localStrategyConfig);
-
 app.get("/", (req, res) => {
   res.json({ msg: "hello from root!" });
 });
 app.use("/blog", routes.blog);
 app.use("/auth", routes.auth);
 app.get("/{*splat}", handleNonExistentRoutes);
-app.listen(3000, () => console.log("app listening on port 3000!"));
+app.listen(port, () => console.log(`app listening on port ${port}`));
